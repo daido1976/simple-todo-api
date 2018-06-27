b6c2adad6c11a4b413f7bc5a23140d77d69792b7
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2018-06-27 19:06:04 +0900

    Todo モデルに NOT NULL 制約を追加

--- db/migrate/20180627100332_create_todos.rb
+++ db/migrate/20180627100332_create_todos.rb
@@ -1,8 +1,8 @@
 class CreateTodos < ActiveRecord::Migration[5.2]
   def change
     create_table :todos, id: :uuid do |t|
-      t.string :title
-      t.string :text
+      t.string :title, null: false
+      t.string :text, null: false
 
       t.timestamps
     end
