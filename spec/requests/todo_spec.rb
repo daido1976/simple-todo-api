08bd1a6b2e8071aa9917340527f59767022e5326
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2018-06-29 13:32:11 +0900

    テスト内で json の値を直接文字列と比較するのではなく、let を介して比較するよう変更

--- spec/requests/todo_spec.rb
+++ spec/requests/todo_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe TodosController, type: :request do
   describe 'GET #index' do
-    let!(:first_todo) { FactoryBot.create(:todo, title: 'first_title', text: 'first_text') }
-    let!(:second_todo) { FactoryBot.create(:todo, title: 'second_title', text: 'second_text') }
+    let!(:first_todo) { FactoryBot.create(:todo) }
+    let!(:second_todo) { FactoryBot.create(:todo) }
 
     it 'HTTP ステータスコード 200 が返ること' do
       get '/todos'
@@ -20,8 +20,8 @@ RSpec.describe TodosController, type: :request do
       get '/todos'
       jsons = JSON.parse(response.body)
       expect(jsons[0]['id']).to eq first_todo.id
-      expect(jsons[0]['title']).to eq 'first_title'
-      expect(jsons[0]['text']).to eq 'first_text'
+      expect(jsons[0]['title']).to eq first_todo.title
+      expect(jsons[0]['text']).to eq first_todo.text
       expect(jsons[0]['created_at']).to eq first_todo.created_at.as_json
     end
 
@@ -29,8 +29,8 @@ RSpec.describe TodosController, type: :request do
       get '/todos'
       jsons = JSON.parse(response.body)
       expect(jsons[1]['id']).to eq second_todo.id
-      expect(jsons[1]['title']).to eq 'second_title'
-      expect(jsons[1]['text']).to eq 'second_text'
+      expect(jsons[1]['title']).to eq second_todo.title
+      expect(jsons[1]['text']).to eq second_todo.text
       expect(jsons[1]['created_at']).to eq second_todo.created_at.as_json
     end
 
