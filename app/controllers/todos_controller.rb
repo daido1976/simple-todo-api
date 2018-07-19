253161e5ac90b8663cd38b3a14dad7a9abf1af3f
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2018-07-19 16:42:41 +0900

    todos_controller に update アクションの追加

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -15,6 +15,12 @@ class TodosController < ApplicationController
     render json: todo
   end
 
+  def update
+    todo = Todo.find(params[:id])
+    todo.update(todo_params)
+    render json: todo
+  end
+
   private
 
   def todo_params
